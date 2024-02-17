cf05c0700a5d78620c1bb9f99eea8173e78f86b9
Rishi Kant <rishi3052000@gmail.com>
2024-02-17 19:22:54 +0530

    add css

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "https://rubygems.org"
 
 ruby "3.2.2"
 
-gem 'bootstrap', '4.4.1'
+gem 'bootstrap', '~> 4.4.1'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3"
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// app/javascript/application.js
+
+// app/javascript/application.js
+
+import 'bootstrap';
+import 'some-other-library';
+import './custom.js';
+import 'jquery';
